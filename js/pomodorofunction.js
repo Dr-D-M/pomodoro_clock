3ff319187d9dfe3474d5b645f9be56f4ad36d784
Dr-D-M <david_garcia.p@hotmail.com>
2018-05-23 18:29:17 -0500

    add js coding

--- js/pomodorofunction.js
+++ js/pomodorofunction.js
@@ -1,2 +1,47 @@
+var sessionWork = 25;
+var breakRest = 5;
+
+// select buttons
+const moreTime = document.querySelectorAll('#upward');
+const lessTime = document.querySelector('#downward');
+//const moreTimeSession = document.querySelector('#upwardsession');
+//const lessTimeSession = document.querySelector('#downwardsession');
+const startTimer = document.querySelector('#play');
+const refreshTimer = document.querySelector('#refresh');
+const stopTimer = document.querySelector('#stop');
+const pauseTimer = document.querySelector('#pause');
+// select display
+var displayTime = document.getElementById('timer');
+var displaySession = document.getElementById('session');
+var displayBreak = document.getElementById('break');
 
 
+// activate  buttons
+
+document.addEventListener('click', function(e){
+console.log(e.target);
+});
+//functions for setting the timing
+function setTimeSession(){
+if(!moreTime)
+}
+
+function setTimeBreak(){
+
+}
+//function to update the displays
+
+function timerUpdateSession(){
+
+}
+
+function timerUpdateBreak(){
+
+}
+function timer(){
+
+}
+
+function refreshTime(){
+
+}
